f3d2641be84395f2aa9115fc27f06dec46032b2e
motegiryosuke <ryousukemotegi@gmail.com>
2016-12-25 21:20:54 +0900

    edit controller to create new group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,12 +3,17 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
-  def edit
+  def create
     @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループが作成されました。"
+    else
+      redirect_to new_group_path , alert: "グループ名を入力してください。"
+    end
   end
 
   private
   def group_params
-    params.require(:group).permit(:name, { user_ids: [] })
+    params.require(:group).permit(:name, {user_ids: []})
   end
 end
