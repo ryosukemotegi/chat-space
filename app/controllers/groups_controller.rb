6115660a18e03b05d17f27967d7129fe5dacbc54
motegiryosuke <ryousukemotegi@gmail.com>
2016-12-25 17:07:11 +0900

    edit groups controller new and edit action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,14 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
+  end
+
+  def edit
+    @group = Group.new(group_params)
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { user_ids: [] })
   end
 end
