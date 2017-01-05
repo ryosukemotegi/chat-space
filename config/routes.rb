38496236fbb4041fbd35f2b766c66c73a514ee05
motegiryosuke <ryousukemotegi@gmail.com>
2017-01-05 21:33:28 +0900

    revise routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
   resources :messages
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
