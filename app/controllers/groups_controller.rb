d8b1440af2582550fe139469db52a7bba0c47134
motegiryosuke <ryousukemotegi@gmail.com>
2017-01-06 21:18:42 +0900

    add set_group  to application controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,10 +27,6 @@ class GroupsController < ApplicationController
   end
 
   private
-  def set_group
-    @group =  Group.find(params[:id])
-  end
-
   def group_params
     params.require(:group).permit(:name, {user_ids: []})
   end
