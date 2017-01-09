62d6a2362662d3fbc2727a0d89d76c25564bd0d7
motegiryosuke <ryousukemotegi@gmail.com>
2017-01-09 21:48:26 +0900

    create send message function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,21 @@
 class MessagesController < ApplicationController
+  before_action :set_group , only: %i(index create)
 
   def index
     @groups = current_user.groups
-    @group = Group.find(params[:group_id])
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id],user_id: current_user.id)
+  end
+
+  def set_group
+    @group =  Group.find(params[:group_id])
   end
 end
