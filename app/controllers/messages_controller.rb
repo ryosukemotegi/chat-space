fb0a00f2c0170bb15026d3d434b86d168c13b262
motegiryosuke <ryousukemotegi@gmail.com>
2017-01-10 17:45:04 +0900

    revise message_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,9 +10,9 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to group_messages_path, notice: "メッセージを投稿しました。"
+      redirect_to group_messages_path(@group), notice: "メッセージを投稿しました。"
     else
-      redirect_to group_messages_path, alert: "メッセージを入力してください。"
+      redirect_to group_messages_path(@group), alert: "メッセージを入力してください。"
     end
 
   end
