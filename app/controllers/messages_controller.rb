2d39ea70fbd0e4dd8ff7b49d54240701c91fc87f
motegiryosuke <ryousukemotegi@gmail.com>
2017-01-11 16:40:54 +0900

    shift error message function from group.index to group.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,12 @@ class MessagesController < ApplicationController
 
   def index
     @groups = current_user.groups
-    @message = @group.messages.new
+    @message = current_user.messages.new
     @messages = @group.messages.includes(:user)
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path(@group), notice: "メッセージを投稿しました。"
     else
@@ -19,7 +19,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id])
   end
 
   def set_group
--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,12 @@ class Group < ApplicationRecord
   has_many :user_groups
   accepts_nested_attributes_for :user_groups
   validates :name, presence: true
+
+  def view_message
+    if messages.present?
+      messages.last.body
+    else
+      "メッセージがありません"
+    end
+  end
 end
