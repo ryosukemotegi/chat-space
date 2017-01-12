9a6d5db5697631bda85fde9f07a5d61e4acf22c7
motegiryosuke <ryousukemotegi@gmail.com>
2017-01-12 14:39:46 +0900

    revise action name of group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,7 +5,7 @@ class Group < ApplicationRecord
   accepts_nested_attributes_for :user_groups
   validates :name, presence: true
 
-  def view_message
+  def last_message
     messages.present? ? messages.last.body : 'メッセージがありません'
   end
 end
