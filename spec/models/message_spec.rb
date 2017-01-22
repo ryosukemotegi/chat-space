eeee9a4e417be10e58c4890604631d51fe362de5
motegiryosuke <ryousukemotegi@gmail.com>
2017-01-22 14:14:41 +0900

    translate comment of example from ja to en

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 describe Message do
   describe '#create' do
-    it "bodyが存在すれば登録できる" do
+    it "is valid with body" do
       message = build(:message)
       expect(message).to be_valid
     end
 
-    it "bodyが空だと登録できない" do
+    it "is invalid without body" do
       message = build(:message, body: nil)
       message.valid?
       expect(message.errors[:body]).to include("を入力してください")
