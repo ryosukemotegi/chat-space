cb3f232464c251b8a665bac0d6d576fc81337fe9
motegiryosuke <ryousukemotegi@gmail.com>
2017-02-01 19:35:06 +0900

    create user search function

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,62 @@
+$(function(){
+  var preName;
+  var preFunc;
+  //ユーザーの検索リスト作成
+  function buildUserList(users) {
+    
+    $(".chat-group-user").remove();
+    var html = "";
+    $.each(users, function(index, user) {
+      html += '<div class="chat-group-user clearfix">' +
+      '<p class="chat-group-user__name">' + user.name + '</p>' +
+      '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="' + user.id + '"data-user-name="' + user.name + '">追加</a></div>';
+    });
+    return html;
+  }
+  //チャットメンバーリストの作成
+  function buildMemberList(id,name){
+    var html = '<div class="chat-group-user clearfix" id="chat-group-user-' + id + '">' +
+    '<input type="hidden" name="group[user_ids][]" value="' + id +'">'+
+    '<p class="chat-group-user__name">' + name + '</p>' +
+    '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="' + id + '">削除</a></div>';
+
+    return html;
+  }
+  //インクリメンタルサーチ
+  $(document).on('keyup', '#user-search-field',function(){
+
+    var name = $(this).val();
+    var ajaxSearch = function(){
+      $.ajax({
+        type: 'get',
+        url: '/users/search.json',
+        data:{
+          keyword: name
+        },
+        dataType: 'json'
+      })
+      .done(function(data) {
+        var html = buildUserList(data);
+        $('#user-search-result').append(html);
+      })
+    };
+    if(name != preName && name.length !== 0){
+      clearTimeout(preFunc);
+      preFunc = setTimeout(ajaxSearch, 500);
+    }
+    preName = name;
+  });
+  //追加ボタンを押した時の処理
+  $(document).on('click','.user-search-add', function(){
+    var id = $(this).attr('data-user-id');
+    var name = $(this).attr('data-user-name');
+    $(this).parent().remove();
+    var html = buildMemberList(id, name);
+    $('#chat-group-users').append(html);
+  });
+
+  //削除ボタンを押した時
+  $(document).on('click', '.user-search-remove', function(){
+    $(this).parent().remove();
+  });
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def search
-    @users = User.where('name LIKE(?)', "%#{params[:name]}%")
+    @users = User.where('name LIKE ?', "%#{params[:keyword]}%")
     respond_to do |format|
       format.json { render json: @users}
+    end
+  end
 end
