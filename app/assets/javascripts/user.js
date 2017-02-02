7cc83846bc3a831914b8aa87a3cd4f41acd72f7a
motegiryosuke <ryousukemotegi@gmail.com>
2017-02-02 15:22:26 +0900

    revise indent of user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -7,17 +7,17 @@ $(function(){
     var html = "";
     $.each(users, function(index, user) {
       html += '<div class="chat-group-user reset-search-list clearfix">' +
-      '<p class="chat-group-user__name">' + user.name + '</p>' +
-      '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="' + user.id + '"data-user-name="' + user.name + '">追加</a></div>';
+              '<p class="chat-group-user__name">' + user.name + '</p>' +
+              '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="' + user.id + '"data-user-name="' + user.name + '">追加</a></div>';
     });
     return html;
   }
   //チャットメンバーリストの作成
   function buildMemberList(id,name){
     var html = '<div class="chat-group-user clearfix" id="chat-group-user-' + id + '">' +
-    '<input type="hidden" name="group[user_ids][]" value="' + id +'">'+
-    '<p class="chat-group-user__name">' + name + '</p>' +
-    '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="' + id + '">削除</a></div>';
+                '<input type="hidden" name="group[user_ids][]" value="' + id +'">'+
+                '<p class="chat-group-user__name">' + name + '</p>' +
+                '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="' + id + '">削除</a></div>';
 
     return html;
   }
