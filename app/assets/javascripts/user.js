8b111aacabe8f5fb2ecf3331ccbc4076d67f5083
motegiryosuke <ryousukemotegi@gmail.com>
2017-02-06 11:33:58 +0900

    revise user.js to improve readability

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -8,7 +8,8 @@ $(function(){
     $.each(users, function(index, user) {
       html += '<div class="chat-group-user reset-search-list clearfix">' +
               '<p class="chat-group-user__name">' + user.name + '</p>' +
-              '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="' + user.id + '"data-user-name="' + user.name + '">追加</a></div>';
+              '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="' +
+              user.id + '"data-user-name="' + user.name + '">追加</a></div>';
     });
     return html;
   }
@@ -17,7 +18,8 @@ $(function(){
     var html = '<div class="chat-group-user clearfix" id="chat-group-user-' + id + '">' +
               '<input type="hidden" name="group[user_ids][]" value="' + id +'">'+
               '<p class="chat-group-user__name">' + name + '</p>' +
-              '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="' + id + '">削除</a></div>';
+              '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="' +
+              id + '">削除</a></div>';
 
     return html;
   }
