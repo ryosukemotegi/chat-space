8251317ad627930960d5b55fe7d140b0fcbac1ec
motegiryosuke <ryousukemotegi@gmail.com>
2017-02-08 17:59:51 +0900

    edit message.js and messages_controller.rb to

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,29 +1,29 @@
 $(function() {
+  //
   function buildHTML(message) {
-    var html = '<div class="chat-message__header">' +
+    var html = '<div class="chat-message__header clearfix">' +
     '<p class="chat-message__name">' + message.name +
-    '<p class="chat-message__time">' + message.creaged_at +
+    '<p class="chat-message__time">' + message.created_at +
     '</div>' +
-    '<p class="chat-message__body">' + message.body;
+    '<p class="chat-message__body">' + message.body +
+    '<br><img src="' + message.image + '">';
     return html;
   }
 
 $('#new_message').on('submit',function(e) {
   e.preventDefault();
-  var textField = $("#message_body");
-  var message = textField.val();
+  var fd = new FormData($("#new_message").get(0));
     $.ajax({
       type: 'POST',
       url: './messages.json',
-      data: {
-        message: {
-          body: message
-        }
-      },
+      data: fd,
+      processData: false,
+      contentType: false,
       dataType: 'json'
     })
     .done(function(data) {
       var html = buildHTML(data);
+
       $('.chat-messages').append(html);
     })
     .fail(function() {
