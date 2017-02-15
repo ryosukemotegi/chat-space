42194adb11900ef8deae2e45f2fda7ce00a5a358
motegiryosuke <ryousukemotegi@gmail.com>
2017-02-15 12:37:02 +0900

    create and edit carrierwave.rb

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,21 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: 'AWS_ACCESS_KEY_ID',
+    aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',
+    region: 'ap-northeast-1'
+  }
+
+  case Rails.env
+    when 'production'
+      config.fog_directory = 'chatspace-image'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace-image'
+
+    when 'development'
+      config.fog_directory = 'chatspace-image'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace-image'
+
+    when 'test'
+      config.storage = :file
+  end
+end
